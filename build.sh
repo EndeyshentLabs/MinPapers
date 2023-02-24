e1fb8f6bc40d29f665d59d7431629c8a5bc868fd
EndeyshentLabs <Themikfound@gmail.com>
2023-02-24 18:13:01 +0300

    Made proper build system

--- build.sh
+++ build.sh
@@ -1,23 +1,45 @@
 #!/usr/bin/env bash
-set -xe
+set -eu
 
 NAME="MinPapers"
 BUILD_DIR="build"
 
-mkdir -p ./$BUILD_DIR
+function need () {
+	if [[ ! $(which $1) ]]; then
+		printf "$1 is needed to build this project!\n"
+		exit 1
+	fi
+}
+
+need 7z
+need wget
+need zip
+need unzip
+
+if [[ ! -d $BUILD_DIR ]]; then
+	mkdir -p ./$BUILD_DIR
+fi
 
 # UNIX
-zip -9 -r ./$NAME.love ./README.md ./LICENSE ./*.lua ./*.otf
+zip -9 -r ./$NAME.love ./README.md ./LICENSE ./*.lua ./res/ ./res/* ./lib ./lib/*
 chmod +x ./$NAME.love
 mv ./$NAME.love ./$BUILD_DIR
 
-# Windows
-cat ~/thirdparty/love-11.4-win64/love.exe ./$BUILD_DIR/$NAME.love > ./$NAME.exe
-cp  ~/thirdparty/love-11.4-win64/*.dll ./$BUILD_DIR/
-chmod +x ./$NAME.exe
-mv ./$NAME.exe ./$BUILD_DIR
+## Windows
+if [[ ! -d love-11.4-win64 ]]; then
+	wget https://github.com/love2d/love/releases/download/11.4/love-11.4-win64.zip
+	unzip love-11.4-win64.zip
+	rm love-11.4-win64.zip*
+fi
+cat ./love-11.4-win64/love.exe ./build/$NAME.love > ./$NAME.exe
+cp  ./love-11.4-win64/*.dll ./build/
+chmod +x ./$NAME.exe # idk why
+mv ./$NAME.exe ./build
 
-# Zip-it
-cd ./$BUILD_DIR
+## Zip-it
+cd ./build
 7z a -tzip ../$NAME.zip .
 cd ..
+
+# Line below is needed for proper syntax highlighting
+# vim:set ft=zsh:
